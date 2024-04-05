c0de8ce874819e66191ef8d44865febec088f47c
Nelson <punkpunkada@gmail.com>
2024-04-05 15:03:05 -0300

    str-producer started

---
+++ Kafka/str-producer/src/main/java/nelsonssoares/strproducer/StrProducerApplication.java
@@ -0,0 +1,13 @@
+package nelsonssoares.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrProducerApplication.class, args);
+	}
+
+}
---
+++ Kafka/str-producer/src/test/java/nelsonssoares/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package nelsonssoares.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
